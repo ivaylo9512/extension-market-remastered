67ba65e04e7df36191945d45538834d8190e2aa0
ivailo aleksandrov <ivayloaleksandrov@yahoo.com>
2020-09-08 23:25:22 +0300

    exceptions deleted

--- server-side/src/main/java/com/tick42/quicksilver/exceptions/ExtensionNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.tick42.quicksilver.exceptions;
-
-public class ExtensionNotFoundException extends RuntimeException {
-    public ExtensionNotFoundException(String exception) {
-        super(exception);
-    }
-}
