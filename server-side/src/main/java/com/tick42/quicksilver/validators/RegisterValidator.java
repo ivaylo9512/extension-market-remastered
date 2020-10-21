f521713385a2a05e8f45ca923138867e979e15bb
ivailo aleksandrov <ivayloaleksandrov@yahoo.com>
2020-10-21 23:34:24 +0300

    RegisterValidator - constant changes + added check for password length

--- server-side/src/main/java/com/tick42/quicksilver/validators/RegisterValidator.java
+++ server-side/src/main/java/com/tick42/quicksilver/validators/RegisterValidator.java
@@ -1,30 +1,34 @@
 package com.tick42.quicksilver.validators;
 
-import com.tick42.quicksilver.models.Spec.ExtensionSpec;
-import com.tick42.quicksilver.models.Spec.UserSpec;
+import com.tick42.quicksilver.models.specs.UserSpec;
 import org.springframework.validation.Errors;
 import org.springframework.validation.ValidationUtils;
 import org.springframework.validation.Validator;
 
-public class UserValidator implements Validator {
-
+public class RegisterValidator implements Validator {
     private static final int MINIMUM_NAME_LENGTH = 7;
     private static final int MAXIMUM_NAME_LENGTH = 22;
+    private static final int MINIMUM_PASSWORD_LENGTH = 9;
+    private static final int MAXIMUM_PASSWORD_LENGTH = 25;
 
     @Override
     public boolean supports(Class<?> clazz) {
-        return ExtensionSpec.class.isAssignableFrom(clazz);
+        return UserSpec.class.isAssignableFrom(clazz);
     }
 
     public void validate(Object target, Errors errors) {
         ValidationUtils.rejectIfEmptyOrWhitespace(errors, "username", "Name must be at least 7 characters long.");
         ValidationUtils.rejectIfEmptyOrWhitespace(errors, "password", "Password must be at least 7 characters long.");
-        UserSpec userSpec = (UserSpec) target;
+
+        UserSpec userSpec = (UserSpec)target;
         if(!userSpec.getPassword().equals(userSpec.getRepeatPassword())){
             errors.reject("Passwords must match.");
         }
         if (errors.getFieldErrorCount("name") == 0 && (userSpec.getUsername().trim().length() < MINIMUM_NAME_LENGTH || userSpec.getUsername().length() > MAXIMUM_NAME_LENGTH)) {
-            errors.reject("Username must be between 7 and 22 characters");
+            errors.reject(String.format("Username must be between %d and %d", MINIMUM_NAME_LENGTH, MAXIMUM_NAME_LENGTH));
+        }
+        if (errors.getFieldErrorCount("password") == 0 && (userSpec.getPassword().trim().length() < MINIMUM_PASSWORD_LENGTH || userSpec.getPassword().length() > MAXIMUM_PASSWORD_LENGTH)) {
+            errors.reject(String.format("Password must be between %d and %d characters", MINIMUM_PASSWORD_LENGTH, MAXIMUM_PASSWORD_LENGTH));
         }
     }
 }
