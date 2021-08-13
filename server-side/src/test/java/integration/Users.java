eb63cbbeb96eb2447470633da920c7d082da6cd6
ivaylo <ivayloaleksandrov@yahoo.com>
2021-08-13 23:55:39 +0300

    Users integration tests added

--- server-side/src/test/java/integration/Users.java
+++ server-side/src/test/java/integration/Users.java
@@ -19,15 +19,22 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.mock.web.MockServletContext;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 import javax.servlet.ServletContext;
 import java.util.Collections;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @ExtendWith(SpringExtension.class)
@@ -69,5 +76,66 @@ public class Users {
         Assert.assertNotNull(servletContext);
         Assert.assertTrue(servletContext instanceof MockServletContext);
         Assert.assertNotNull(webApplicationContext.getBean("userController"));
+    }
+
+    @WithMockUser(value = "spring")
+    @Test
+    public void register() throws Exception {
+        RequestBuilder request = post("/api/users/register")
+                .param("username", "username")
+                .param("password", "password")
+                .param("repeatPassword", "password");
+
+        mockMvc.perform(request)
+                .andExpect(status().isOk());
+    }
+
+    @WithMockUser(value = "spring")
+    @Test
+    public void registerAdmin() throws Exception {
+        RequestBuilder request = post("/api/users/auth/registerAdmin")
+                .header("Authorization", adminToken)
+                .param("username", "username1")
+                .param("password", "password")
+                .param("repeatPassword", "password");
+
+        mockMvc.perform(request)
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void register_WhenUsernameIsTaken() throws Exception {
+        RequestBuilder request = post("/api/users/register")
+                .param("username", "username")
+                .param("password", "password")
+                .param("repeatPassword", "password");
+
+        mockMvc.perform(request)
+                .andExpect(content().string(containsString("Username is already taken.")));
+    }
+
+    @Test
+    public void login() throws Exception {
+        mockMvc.perform(post("/api/users/login")
+                .contentType("Application/json")
+                .content("{\"username\": \"username\", \"password\": \"password\"}")).andExpect(status().isOk());
+    }
+
+    @Test
+    public void login_WithWrongPassword_ShouldThrow() throws Exception {
+        mockMvc.perform(post("/api/users/login")
+                .contentType("Application/json")
+                .content("{\"username\": \"username\", \"password\": \"incorrect\"}"))
+                .andExpect(status().is(401))
+                .andExpect(content().string(containsString("Bad credentials")));
+    }
+
+    @Test
+    public void login_WithWrongUsername_ShouldThrow() throws Exception {
+        mockMvc.perform(post("/api/users/login")
+                .contentType("Application/json")
+                .content("{\"username\": \"incorrect\", \"password\": \"password\"}"))
+                .andExpect(status().is(401))
+                .andExpect(content().string(containsString("Bad credentials")));
     }
 }
