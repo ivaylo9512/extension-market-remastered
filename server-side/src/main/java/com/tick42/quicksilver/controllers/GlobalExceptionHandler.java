b0357d41cdad6e082e3495b9aec09e6ed01767f2
ivaylo <ivayloaleksandrov@yahoo.com>
2021-08-28 01:49:10 +0300

    EntityUnavailableException removed + BindException/MethodArgumentNotValidException handlers because ResponseEntityExceptionHandler is already handling them

--- server-side/src/main/java/com/tick42/quicksilver/controllers/GlobalExceptionHandler.java
+++ server-side/src/main/java/com/tick42/quicksilver/controllers/GlobalExceptionHandler.java
@@ -1,14 +1,10 @@
 package com.tick42.quicksilver.controllers;
 
 import com.tick42.quicksilver.exceptions.*;
-import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.BindException;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 import javax.persistence.EntityNotFoundException;
 
@@ -28,19 +24,6 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
                 .body(e.getMessage());
     }
 
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    @ResponseBody
-    public ResponseEntity<Object> handleInvalidSpecException(MethodArgumentNotValidException e) {
-        e.printStackTrace();
-        return ResponseEntity
-                .status(HttpStatus.BAD_REQUEST)
-                .body(e.getBindingResult()
-                        .getFieldErrors()
-                        .stream()
-                        .map(DefaultMessageSourceResolvable::getDefaultMessage)
-                        .toArray());
-    }
-
     @ExceptionHandler
     ResponseEntity<String> handleInvalidInputException(InvalidInputException e){
         return ResponseEntity
@@ -52,24 +35,6 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
     ResponseEntity<String> handleFileFormatException(FileFormatException e){
         return ResponseEntity
                 .status(HttpStatus.BAD_REQUEST)
-                .body(e.getMessage());
-    }
-
-    @ExceptionHandler
-    ResponseEntity<Object> handleBindException(BindException e){
-        return ResponseEntity
-                .status(HttpStatus.BAD_REQUEST)
-                .body(e.getBindingResult().getAllErrors()
-                        .stream()
-                        .map(DefaultMessageSourceResolvable::getCode)
-                        .toArray());
-    }
-
-    @ExceptionHandler
-    ResponseEntity<String> handleEntityUnavailable(EntityUnavailableException e) {
-        e.printStackTrace();
-        return ResponseEntity
-                .status(HttpStatus.UNAUTHORIZED)
                 .body(e.getMessage());
     }
 
--- server-side/src/main/java/com/tick42/quicksilver/exceptions/EntityUnavailableException.java
+++
@@ -1,8 +0,0 @@
-package com.tick42.quicksilver.exceptions;
-
-public class EntityUnavailableException extends RuntimeException {
-
-    public EntityUnavailableException(String exception) {
-        super(exception);
-    }
-}
