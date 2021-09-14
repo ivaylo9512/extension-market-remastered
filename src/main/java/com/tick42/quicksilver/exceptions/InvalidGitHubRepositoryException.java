71e6de9b1098ddf78d0abb03ab6dbb2970f10aa1
ivaylo <ivayloaleksandrov@yahoo.com>
2021-09-14 09:17:19 +0300

    ExtensionsData sql added + InvalidGitHubRepositoryException/GenerateTokenException deleted

--- src/main/java/com/tick42/quicksilver/exceptions/InvalidGitHubRepositoryException.java
+++
@@ -1,7 +0,0 @@
-package com.tick42.quicksilver.exceptions;
-
-public class InvalidGitHubRepositoryException extends RuntimeException {
-    public InvalidGitHubRepositoryException(String exception) {
-        super(exception);
-    }
-}
